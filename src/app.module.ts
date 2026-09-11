f23b0c5a286a419ad4a98791bdb55152f0b4be10
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-09-10 19:23:47 -0600

    add: Prisma ORM Ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { CategoriesModule } from './categories/categories.module';
+import "dotenv/config";
 
 @Module({
   imports: [RolesModule, CategoriesModule], 
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class CategoriesService {
+
+  constructor(private prisma: PrismaService) {}
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll() {
+    return await this.prisma.categorias.findMany();
   }
 
   findOne(id: number) {
