ba87c7aa9aceb175a9078de2ee7e0f206bc80e37
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-09-11 11:56:01 -0600

    add: simple roles crud ready

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { RolesService } from './roles.service';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+
+    constructor(
+        private rolesService: RolesService
+    ){}
+
+    @Post()
+    createRole(@Body() role: any){
+        return this.rolesService.createRole(role);
+    }
+
+    @Get()
+    getAllRoles(){
+        return this.rolesService.getAllRoles();
+    }
+
+    @Get(':id')
+    getRoleById(@Param('id') id:string){
+        return this.rolesService.getRoleById(+id)//+id convierte el paremetro id de string a number
+    }
+
+    @Patch(':id')
+    updateRole(@Param('id') id:string, @Body() role: any){
+        return this.rolesService.updateRole(+id, role)
+    }
+
+    @Delete(':id')
+    deleteRole(@Param('id') id: string){
+        return this.rolesService.deleteRole(+id);
+    }
+    
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+    constructor(
+        private prisma: PrismaService
+    ) { }
+
+    async createRole(role: any){
+        return await this.prisma.roles.create({
+            data: role
+        })
+    }
+
+    async getAllRoles() {
+        return await this.prisma.roles.findMany();
+    }
+
+    async getRoleById(id: number){
+        return await this.prisma.roles.findUnique({
+            where: {
+                id: id,
+            }
+        })
+    }
+
+    async updateRole(id: number,role: any) {
+        return await this.prisma.roles.update({
+            where: {
+                id: id
+            },
+            data: role,
+        })
+    }
+
+    async deleteRole(id: number){
+        return await this.prisma.roles.delete({
+            where: {
+                id: id,
+            }
+        })
+
+    }
+}
