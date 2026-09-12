409de0b6b3e673084e3106b6947ed4675619ad41
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-09-11 18:28:27 -0600

    add: category crud and validation fields

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -7,23 +7,45 @@ import { PrismaService } from 'src/prisma.service';
 export class CategoriesService {
 
   constructor(private prisma: PrismaService) {}
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return await this.prisma.categorias.create({
+      data: createCategoryDto
+    });
   }
 
   async findAll() {
     return await this.prisma.categorias.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.prisma.categorias.findUnique({
+      where: {
+        id: id
+      }
+    });
+    if(!category) {
+      return new NotFoundException(`Category with id ${id} not found`);
+    }
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return await this.prisma.categorias.update({
+      where: {
+        id: id
+      },
+      data: updateCategoryDto
+    })
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    await this.prisma.categorias.delete({
+      where: {
+        id: id
+      }
+    })
   }
+
 }
--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateCategoryDto {
+
+    @IsNotEmpty()
+    @IsString()
+    nombre!: string;
+
+    @IsString()
+    @IsOptional()
+    descripcion?: string;
+}
