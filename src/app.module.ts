7b84aedbc3daefd34edeeea58a0ab6e6b2800694
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-09-14 12:49:34 -0600

    Add: Apointment module. Apointment status module with its apis ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RolesModule } from './roles/roles.module';
-import { CategoriesModule } from './categories/categories.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import "dotenv/config";
+import { AppointmentModule } from './modules/appointments/appointment/appointment.module';
+import { AppointmentDetailModule } from './modules/appointments/appointment-detail/appointment-detail.module';
+import { AppointmentStatusModule } from './modules/appointments/appointment-status/appointment-status.module';
 
 @Module({
-  imports: [RolesModule, CategoriesModule], 
+  imports: [
+    RolesModule, 
+    CategoriesModule,
+    AppointmentModule,
+    AppointmentStatusModule,
+    AppointmentDetailModule
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
